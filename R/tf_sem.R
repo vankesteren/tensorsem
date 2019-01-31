8b3e2d4e8c6d460b57e3a67ff258847624134207
vankesteren <erikjanvankesteren@pm.me>
2019-01-31 13:09:54 +0100

    add example to tf_sem documentation

--- R/tf_sem.R
+++ R/tf_sem.R
@@ -70,6 +70,13 @@
 #'   }
 #' }
 #'
+#' @examples
+#' mod    <- "x1 ~ x2 + x3"
+#' dat    <- lavaan::HolzingerSwineford1939
+#' tf_mod <- tf_sem(mod, dat)
+#' tf_mod$train(2000)
+#' tf_mod$summary()
+#'
 #' @export
 tf_sem <- function(lav_model, data, fit = FALSE) {
 
